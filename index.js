2b74b05f669c1b7dba845c82f7c4f3ac512c515c
ahmed youssef elsayed <ahmedyoussef59@yahoo.com>
2018-03-31 19:48:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ function destructivelyAppendKitten(name) {
 function destructivelyPrependKitten(name) {
   return kittens.unshift(name);
 }
+function destructivelyRemoveLastKitten() {
+  return kittens.pop.();
+}
 
 
